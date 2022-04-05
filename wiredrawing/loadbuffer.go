6788147e02955a9477d5887c172734b13f164d30
Senbiki-Akifumi <a-senbiki@sunmark.bz>
2022-04-05 13:04:43 +0900

    コマンド実行後の出力のバッファを1より大きくする場合の検証

--- wiredrawing/loadbuffer.go
+++ wiredrawing/loadbuffer.go
@@ -14,30 +14,44 @@ var currentLine int
 
 // ---------------------------------------------------------------------
 // 引数に渡された io.ReadCloser 変数の中身を読み取り出力する
+// 2060
 // ---------------------------------------------------------------------
 func LoadBuffer(buffer io.ReadCloser, previousLine *int) (bool, error) {
 	// fmt.Println("bufferの読み取り開始------>")
 	currentLine = 0
 
+	var ensureLength = 1024
+	fmt.Println("*previousLine", *previousLine)
 	for {
-		readData := make([]byte, 1)
+		readData := make([]byte, ensureLength)
 		n, err := buffer.Read(readData)
 
 		if err != nil {
-			// fmt.Println(err)
 			break
 		}
 
 		if n == 0 {
 			break
 		}
-		if currentLine >= *previousLine {
-			fmt.Print(string(readData))
+
+		if (currentLine + n) >= *previousLine {
+
+			if currentLine < *previousLine && *previousLine < (currentLine+n) {
+				fmt.Print(string(readData[(*previousLine - currentLine):]))
+			} else {
+				fmt.Print(string(readData))
+			}
 		}
-		currentLine++
+		// if currentLine >= *previousLine {
+		// 	//2048 2060
+		// 	fmt.Print(string(readData))
+		// }
+		currentLine += n
 		readData = nil
 	}
 	*previousLine = currentLine
 
+	// fmt.Println("*previousLine", *previousLine)
+	// fmt.Println("currentLine", currentLine)
 	return true, nil
 }
